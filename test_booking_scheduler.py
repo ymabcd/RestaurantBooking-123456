bf70250f57ca7bf0ca2a7b59d3ed8fc2f9c3bf98
ymin86kim <ymin86kim@gmail.com>
2024-06-14 11:31:52 +0900

    Step7

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -16,6 +16,8 @@ CAPACITY_PER_HOUR = 3
 class BookingSchedulerTest(unittest.TestCase):
     def setUp(self):
         self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
+        self.testable_sms_sender = TestableSmsSender()
+        self.booking_scheduler.set_sms_sender(self.testable_sms_sender)
 
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
         schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
@@ -52,13 +54,11 @@ class BookingSchedulerTest(unittest.TestCase):
         self.assertTrue(self.booking_scheduler.has_schedule(new_schedule))
 
     def test_예약완료시_SMS는_무조건_발송(self):
-        testable_sms_sender = TestableSmsSender()
         schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
-        self.booking_scheduler.set_sms_sender(testable_sms_sender)
 
         self.booking_scheduler.add_schedule(schedule)
 
-        self.assertTrue(testable_sms_sender.is_send_method_is_called())
+        self.assertTrue(self.testable_sms_sender.is_send_method_is_called())
 
     def test_이메일이_없는_경우에는_이메일_미발송(self):
         pass
