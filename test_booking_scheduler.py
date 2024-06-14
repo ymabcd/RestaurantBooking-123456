08cb52373d97d5d6a946c613983da8134fdfe125
ymin86kim <ymin86kim@gmail.com>
2024-06-14 13:41:41 +0900

    Step1

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -4,11 +4,15 @@ from datetime import datetime, timedelta
 from booking_scheduler import BookingScheduler
 from communication_test import TestableSmsSender, TestableMailSender
 from schedule import Customer, Schedule
+from unittest.mock import Mock, patch
 
 NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
 ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
-CUSTOMER = Customer("Fake name", "010-1234-5678")
-CUSTOMER_WITH_MAIL = Customer("Fake name", "010-1234-5678", "test@test.com")
+CUSTOMER = Mock()
+CUSTOMER.get_email.return_value = None
+CUSTOMER_WITH_MAIL = Mock()
+CUSTOMER_WITH_MAIL.get_email.return_value = "test@test.com"
+
 
 UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
@@ -92,7 +96,6 @@ class BookingSchedulerTest(unittest.TestCase):
             new_schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL)
             self.booking_scheduler.add_schedule(new_schedule)
             self.fail()
-
 
     def test_현재날짜가_일요일이_아닌경우_예약가능(self):
         self.booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, "2024/06/03 17:00")
